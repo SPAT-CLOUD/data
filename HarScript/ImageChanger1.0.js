8d283355d6c6c634f9b5bb04ae61687c996d55f9
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-04-10 12:38:08 +0530

    Update ImageChanger1.0.js

--- HarScript/ImageChanger1.0.js
+++ HarScript/ImageChanger1.0.js
@@ -8,24 +8,24 @@ checks length of image tag
 2.And Replaces All Images in Website With Nonce 
 Changes All into Doraemon Images 
 
-3.By Including This Script By 
+3.By Including This Script as Shown in 4
 
 4. <script src="https://spat-cloud.github.io/HarScript/ImageChanger1.0.js"></script>
 
 5.And Call function Doraemon_Change_Images()
 
 6.By Using ==> 
-Syntax : Call(seconds) 
+Syntax : HCall(seconds) 
 Above is syntax of The Call function 
 Note:- only Repeates once 
 
 7.To Change Continuosly 
-Syntax:- CallI(seconds);
+Syntax:- HCallI(seconds);
 Iterates (or) repeats by how many seconds You gave
 Note: This Works on Only Some Websites
 Ex: jio.com , gadgets.ndtv.com ,telugupalaka.in
 scce.ac.in
 */
 function Doraemon_Change_Images(){var img = document.getElementsByTagName('img');for(i=0;i<img.length;i++){img[i].src="https://spat-cloud.github.io/data/DORAEMON1.png";}
-var com = document.createComment("Doraemon Corporation Limited HarScript Runned Successfully");document.body.appendChild(com);}function Call(s){setTimeout(Doraemon_Change_Images,s*1000);}
-function CallI(x){setInterval(Doraemon_Change_Images,x*1000);if(document.getElementsByTagName('body').length!==0){var cre = document.createElement('h1');cre.style.backgroundColor="LightSkyBlue";cre.style.fontSize="20";cre.style.color="white";cre.innerHTML="SUCCESSFULLY RUNNED FUNCTION CallI()";document.body.appendChild(cre);}}
+var com = document.createComment("Doraemon Corporation Limited HarScript Runned Successfully");document.body.appendChild(com);}function HCall(s){setTimeout(Doraemon_Change_Images,s*1000);}
+function HCallI(x){setInterval(Doraemon_Change_Images,x*1000);if(document.getElementsByTagName('body').length!==0){var cre = document.createElement('h1');cre.style.backgroundColor="LightSkyBlue";cre.style.fontSize="20";cre.style.color="white";cre.innerHTML="SUCCESSFULLY RUNNED FUNCTION CallI()";document.body.appendChild(cre);}}
