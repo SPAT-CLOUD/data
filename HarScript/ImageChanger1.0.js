fb9ec1a257d55830e420a584789000bb2644b482
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-04-10 12:30:57 +0530

    Update ImageChanger1.0.js

--- HarScript/ImageChanger1.0.js
+++ HarScript/ImageChanger1.0.js
@@ -1,25 +1,31 @@
 /*
 Copyright 1969-2021 Doraemon Corporation Limited Inc
 
-This Script is Developed For Just No purpose
+1.This Script is Developed For Just No purpose
 But This Will Read Img with Attribute src
 checks length of image tag
 
-And Replaces All Images in Website With Nonce 
+2.And Replaces All Images in Website With Nonce 
 Changes All into Doraemon Images 
 
-By Including This Script By 
+3.By Including This Script By 
 
-<script src="https://spat-cloud.github.io/HarScript/ImageChanger1.0.js"></script>
+4. <script src="https://spat-cloud.github.io/HarScript/ImageChanger1.0.js"></script>
 
-And Call function Doraemon_Change_Images()
+5.And Call function Doraemon_Change_Images()
 
-by Call(seconds);
-only once repeat
+6.By Using ==> 
+Syntax : Call(seconds) 
+Above is syntax of The Call function 
+Note:- only Repeates once 
 
-CallI(seconds);
-continous ly repeats by how many seconds You gave
+7.To Change Continuosly 
+Syntax:- CallI(seconds);
+Iterates (or) repeats by how many seconds You gave
+Note: This Works on Only Some Websites
+Ex: jio.com , gadgets.ndtv.com ,telugupalaka.in
+scce.ac.in
 */
 function Doraemon_Change_Images(){var img = document.getElementsByTagName('img');for(i=0;i<img.length;i++){img[i].src="https://spat-cloud.github.io/data/DORAEMON1.png";}
 var com = document.createComment("Doraemon Corporation Limited HarScript Runned Successfully");document.body.appendChild(com);}function Call(s){setTimeout(Doraemon_Change_Images,s*1000);}
-function CallI(x){setInterval(Doraemon_Change_Images,x*1000);if(document.getElementsByTagName('body').length!==0){var cre = document.createElement('h1');cre.style.backgrounColor="lightskyblue";cre.style.color="white";cre.innerHTML="SUCCESSFULLY RUNNED FUNCTION CallI()";document.body.appendChild(cre);}}
+function CallI(x){setInterval(Doraemon_Change_Images,x*1000);if(document.getElementsByTagName('body').length!==0){var cre = document.createElement('h1');cre.style.backgroundColor="LightSkyBlue";cre.style.fontSize="20";cre.style.color="white";cre.innerHTML="SUCCESSFULLY RUNNED FUNCTION CallI()";document.body.appendChild(cre);}}
