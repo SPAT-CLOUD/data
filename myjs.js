4313bb60736e7205572c6ff64ccfac70cf7e67a0
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-04-12 12:56:34 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -4,10 +4,14 @@ let UsAg = navigator.userAgent;
  Device.value=`${UsAg}`;
 let Nt = document.getElementById("NetTyp");
 let nn = navigator.connection.type;
-
+var val = document.getElementById("Val2");
+var not = document.getElementById("Notf2");
 Nt.value=`${nn}`;
-if(ht.value==''||ht.value==null){
+if(ht.value==''||ht.value==null||ht.length!==10||val.length==8||val.value==""||val.value==null){
     var d = document.getElementById("Notf");
     d.style.display="block";
+}
+else{
+not.style.display="block";
 }
 }
