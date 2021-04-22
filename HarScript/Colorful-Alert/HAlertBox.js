706b2cf0b13f82dcc06be7c53e6d61fbc48fb7e6
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-04-22 20:02:45 +0530

    Add files via upload

---
+++ HarScript/Colorful-Alert/HAlertBox.js
@@ -0,0 +1,14 @@
+function Close(My){
+  My.parentElement.parentElement.style.display="none";
+    
+}
+function HAlertBox(){
+var Ty = document.getElementById("HAlertBox");
+ var der = document.getElementById("InnerHAlertBox");
+
+ 
+ Ty.style.display="block";  
+ 
+    der.style.animation="comed 2s 1";
+   
+        }
