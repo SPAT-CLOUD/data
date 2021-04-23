cd3f0d68320ee5ca55485795b6df9ce7e98ab9ad
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-04-23 11:02:25 +0530

    Update HAlertBoxCreater-v1.1.js

--- HarScript/Colorful-Alert/HAlertBoxCreater-v1.1.js
+++ HarScript/Colorful-Alert/HAlertBoxCreater-v1.1.js
@@ -1,9 +1,49 @@
+/*
+* CopyRight © 2021 Doraemon Corporation , Inc Limited
+* This Code Genrates Colorful Alert Rather Than
+* Standard alert(); Function Provided By JavaScript
+* The Default Property Has Two Buttons 1st LeftButton Close
+* The Close Button Closes 
+* The Second Button Right Button Ok Also Closes
+* The Default Color of Alert box is White
+* The Default Color Of Buttons is Green
+* First Call Function CreateAlertBox();
+* Then Use HAlertBox(YourStatments);
+* Example HAlertBox("Test");
+* To Use This In Your Website First thing
+* You Must Have <head> and <body> tags
+* In head tag include as shown below
+* <script type="text/javascript" src="https://spat-cloud.github.io/data/HarScript/Colorful-Alert/HAlertBoxCreater-v1.1.js"></script>
+* Next Call Function CreateAlertBox();
+* <script> HAlertBox(YourStaments);</script>
+* Ex:
+  <!DOCTYPE HTML>
+<html>
+    <head>
+        <title>
+            Test
+        </title>
+ <script src="https://spat-cloud.github.io/data/HarScript/Colorful-Alert/HAlertBoxCreater-v1.1.js"></script> 
+ <meta name="viewport" content="width=device-width, Maximum-scale=1 , initial-scale=1.0">
+    </head>
+    <script>
+  function Test(){
+      HAlertBox("html");
+  }
+    </script>
+    <body onload="CreateAlertBox()">
+        
+  <button onclick="Test()">Click</button>      
+    </body>
+</html>
+*/
+
 function CreateAlertBox(){
 var LinkRel = document.createElement('link');
-LinkRel.rel="stylesheet";
-LinkRel.type="text/css";
-LinkRel.href="https://spat-cloud.github.io/data/HarScript/Colorful-Alert/HAlertBoxStyle.css";
-document.head.appendChild(LinkRel);
+   LinkRel.rel="stylesheet";
+   LinkRel.type="text/css";
+   LinkRel.href="https://spat-cloud.github.io/data/HarScript/Colorful-Alert/HAlertBoxStyle.css";
+     document.head.appendChild(LinkRel);
 var HAlertBox = document.createElement('div');
 HAlertBox.id="HAlertBox";
 document.body.appendChild(HAlertBox);
