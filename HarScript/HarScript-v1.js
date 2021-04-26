c751cb1c97ae97aa7091632c048d171967075656
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-04-26 15:13:05 +0530

    Update HarScript-v1.js

--- HarScript/HarScript-v1.js
+++ HarScript/HarScript-v1.js
@@ -1,3 +1,14 @@
+/*
+* Copyright © To Doraemon Corporation.
+* This is known as Harsha Script.
+* This developed for Modal Alerts Colorful Alert Boxes
+* The Function In this genrates Colorful Alert 
+
+
+
+
+
+*/
 function CreateAlertBox(){var LinkRel = document.createElement('link');LinkRel.rel="stylesheet";LinkRel.type="text/css";LinkRel.href="https://spat-cloud.github.io/data/HarScript/Colorful-Alert/HAlertBoxStyle.css";document.head.appendChild(LinkRel);var HAlertBox = document.createElement('div');HAlertBox.id="HAlertBox";document.body.appendChild(HAlertBox);var InnerHAlertBox = document.createElement('span');InnerHAlertBox.id="InnerHAlertBox";HAlertBox.appendChild(InnerHAlertBox);var Para = document.createElement('p');Para.id="Content";InnerHAlertBox.appendChild(Para);var LeftButtonClose = document.createElement('button');LeftButtonClose.id="LeftButtonClose";LeftButtonClose.innerHTML="Close";LeftButtonClose.onclick=function(){Close(this)};InnerHAlertBox.appendChild(LeftButtonClose);var RightButtonOk = document.createElement('button');RightButtonOk.id="RightButtonOk";RightButtonOk.innerHTML="OK";RightButtonOk.onclick=function(){Close(this)};InnerHAlertBox.appendChild(RightButtonOk);}function HCallOnce(YourFunc,TimeSec){setTimeout(YourFunc,TimeSec*1000);}HCallOnce(CreateAlertBox,1);
 function HAlertBox(html){var Ty = document.getElementById("HAlertBox");var der = document.getElementById("InnerHAlertBox");var AlertHtml = document.getElementById("Content");Ty.style.display="block";  der.style.animation="comed 2s 1";AlertHtml.innerHTML=html;}
 function Close(My){My.parentElement.parentElement.style.display="none";}
