f9e663093c601457e69210270cccd106f18b972e
sonpanabtej <hv2471920@gmail.com>
2021-05-19 20:41:53 +0530

    Update Advertising.js

--- HarScript/Advertising.js
+++ HarScript/Advertising.js
@@ -53,7 +53,8 @@ Q.setAttribute("style","color:White;Background-Image:Linear-gradient(to Bottom ,
 Q.innerHTML=Bmsg;
 Hf.appendChild(Q);}
 function Vf(x){x.parentElement.parentElement.style.display="none";}
-function start(){
-    setTimeout(()  => HAdv("https://i.ytimg.com/vi_webp/I3smzfz5oe4/mqdefault.webp" , "Watch Shinchan Movie Golden Sword Deleted Scenes On Youtube","https://m.youtube.com/watch?v=I3smzfz5oe4&list=PLfiin_UZFjZR1YIsh3S9iPQVE2vi3MgQ1&index=1" , "Watch Now For Free"), 4000);
+/*function start(){
+    setTimeout(()  => 
+HAdv("https://i.ytimg.com/vi_webp/I3smzfz5oe4/mqdefault.webp" , "Watch Shinchan Movie Golden Sword Deleted Scenes On Youtube","https://m.youtube.com/watch?v=I3smzfz5oe4&list=PLfiin_UZFjZR1YIsh3S9iPQVE2vi3MgQ1&index=1" , "Watch Now For Free"), 4000);
 }
-start();
+start();*/
