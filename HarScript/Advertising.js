f2829c23957ff17a62a4da26026611effb42d4e7
sonpanabtej <hv2471920@gmail.com>
2021-05-31 11:19:56 +0530

    Update Advertising.js

--- HarScript/Advertising.js
+++ HarScript/Advertising.js
@@ -50,6 +50,6 @@ Q.innerHTML=Bmsg;
 Hf.appendChild(Q);}
 function Vf(x){x.parentElement.parentElement.style.display="none";}
 function start(){
-    setTimeout(()  => HAdv("https://i9.ytimg.com/vi_webp/TYQeLve5S0o/mqdefault.webp?time=1622439600000&sqp=CLDt0YUG&rs=AOn4CLBEgblI8HtfMd64aR0buGjwG6_9UA" , "Watch Shinchan Movie Golden Sword Deleted Scenes On Youtube","https://m.youtube.com/watch?v=I3smzfz5oe4&list=PLfiin_UZFjZR1YIsh3S9iPQVE2vi3MgQ1&index=1" , "Watch Now For Free"), 4000);
+    setTimeout(()  => HAdv("https://spat-cloud.github.io/data/HarScript/Shinchan-Movie-16-DelScene-2.webp" , "Crayon Shin chan Hero Of Kinpoko   Movie 16 2008 1080p Deleted scene  002","https://youtu.be/pkS-Q97ctOc" , "Watch Now For Free"), 4000);
 }
 start();
