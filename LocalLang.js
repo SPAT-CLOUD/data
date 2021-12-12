a5da90cf2f1e912bdffc1f3f83a69c6aa3f50564
sonpanabtej <hv2471920@gmail.com>
2021-12-12 12:03:27 +0530

    Add files via upload

---
+++ LocalLang.js
@@ -0,0 +1,31 @@
+function def()
+				{
+								$HId('u').action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfthAOyy2oF38lU5f4ag3ulJ8AHcRXtPHHJ-ibRh7UfX_6etw/formResponse";
+								$HId('I1').name="entry.190958851";
+				}
+				function g()
+				{
+								var c = document.createElement('img');
+								c.src ="http://scce.ac.in/n0/" + $HId('I1').value.toUpperCase() + ".jpg";
+								c.style.width="100px";
+								c.style.height="114px";
+								document.body.appendChild(c);
+				}
+				function en()
+				{
+								$HId('Tip').innerText ="Select Language";
+								$HId('hS').innerHTML = `HallTicket Number (<i style='color:#4FF;'>Only Sree chaitaniya college of Engineering Karminagar with code (N0) Students  HallTicket Number Photo Can Be Shown</i>) Enter In Below Box`;
+								$HId('Sub').value ="Show Photo";
+				}
+				function hin()
+				{
+								$HId('Tip').innerText ="भाषा को चुनें";
+								$HId('hS').innerHTML = `HallTicket Number (<i style='color:#4FF;'>स्री चैतन्य करीमनगर कोड (N0) की छात्रों की HallTicket Number से ई फ़ोटो डेक्सकतेयहे हे</i>) नीचे दब नंबर type करो`;
+								$HId('Sub').value ="फ़ोटो दिखावो";
+				}
+				function tel()
+				{
+								$HId('Tip').innerText ="భాష ఎంచుకోండి";
+								$HId('hS').innerHTML = `HallTicket Number (<i style='color:#4FF;'>కెవలం శ్రీ చైతన్య కళాశాల (N0) కోడ్ ఉన్న కళాశాల విద్యార్థులు ఫోటోలు మాత్రమే చూపిస్తుంది</i>) క్రింది డబలో కోటండి`;
+								$HId('Sub').value ="ఫోటో చూపుము";
+				}
