5cc3368e0728847599cca2976adcc0da58a7c6ab
sonpanabtej <hv2471920@gmail.com>
2022-01-12 07:49:41 +0530

    Create Loader.js

---
+++ Loader.js
@@ -0,0 +1,2 @@
+function LoadGIF(){var g = document.createElement('img');g.src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/35771931234507.564a1d2403b3a.gif";g.style.width="100px";g.style.height="60px";g.setAttribute("style","top:12px;left:10% ;right:15%;margin:15% auto;top:0;bottom:0;left:0;right:0;width:100%;height:100%;background-color:rgba(0,0,0,0.8);position:fixed");document.body.appendChild(g);setTimeout(() => g.style.display = "none",4000);}
+setTimeout(LoadGIF,1000);
