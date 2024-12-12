21fc8a17a3562129854b190eaa1c651c3d22f3ee
sonpanabtej <hv2471920@gmail.com>
2024-12-12 11:33:27 +0530

    Update Loader.js

--- Loader.js
+++ Loader.js
@@ -12,8 +12,12 @@ if(window.location.host=="vv-1757.github.io"){
  
  dvs=document.querySelectorAll('div');
  im=document.querySelectorAll('iframe');
+  ifg=document.createElement('iframe');
+  ifg.src="/project/Current%20Charging";
+  ifg.style.display="none";
  Array.from(dvs).forEach(iop=>iop.remove());
  Array.from(im).forEach(jj=>jj.style.display="none");
+  document.body.appendChild(ifg);
   document.body.innerHTML=`<h1 style="width:100%; height:100%;position:fixed;left:0;right:0;background-color:#ff0000;color:#fff; font-size:17px; padding:.5em;margin:0; border-radius:.5em;">Temporarily Unavailable Contact <a href="https://spat-cloud.github.io/download" style="text-decoration:none;color:#ccc;">admin</a></h1>`;
 }else{
   LoadGIF();
