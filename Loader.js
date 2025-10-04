8fcbb62e380b1caed1beb30ae79f6bbf0aed0464
sonpanabtej <hv2471920@gmail.com>
2025-10-04 13:44:41 +0530

    Update Loader.js

--- Loader.js
+++ Loader.js
@@ -7,18 +7,9 @@ function LoadGIF(){
   g.setAttribute("style","margin:1% auto;top:0;bottom:0;left:0;right:0;width:100%;height:100%;background-color:rgba(0,0,0,0.8);position:fixed");document.body.appendChild(g);
   setTimeout(() => g.style.display = "none",8000);
 }
-function xyza(){
-if(window.location.host=="vv-1757.github.io"){
- 
- dvs=document.querySelectorAll('div');
- im=document.querySelectorAll('iframe');
-  ifg=document.createElement('iframe');
-  ifg.src="/project/Current%20Charging";
-  ifg.style.display="none";
- Array.from(dvs).forEach(iop=>iop.remove());
- Array.from(im).forEach(jj=>jj.style.display="none");
-  document.body.appendChild(ifg);
-  document.body.innerHTML=`<h1 style="width:100%; height:100%;position:fixed;left:0;right:0;background-color:#ff0000;color:#fff; font-size:17px; padding:.5em;margin:0; border-radius:.5em;">Temporarily Unavailable Contact <a href="https://spat-cloud.github.io/download" style="text-decoration:none;color:#ccc;">admin</a></h1>`;
+async function xyza(){
+if(window.location.host.includes("vv-1757")){
+window.open("https://spat-cloud.github.io/new/") 
 }else{
   LoadGIF();
 }}
